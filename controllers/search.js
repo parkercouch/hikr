2859ad0464c7e3a800a5c784e9336d3d45a4d8e8
Parker Couch <parkercouch@gmail.com>
2019-01-16 12:08:43 -0800

    Search view/route added

--- controllers/search.js
+++ controllers/search.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
+// DATABASE
+const db = require('../models');
+
 // MIDDLEWARE
 const loggedIn = require('../middleware/loggedIn');
 
 // GET /search -- show user profile
 router.get('/', loggedIn, (req, res) => {
-  res.send('search');
+  db.profile.findAll().then((foundUsers) => {
+    const nextUser = foundUsers[Math.floor(Math.random() * foundUsers.length)];
+    res.render('search/search', { nextUser });
+  }).catch((err) => {
+    console.log(err);
+  });
 });
 
 module.exports = router;
