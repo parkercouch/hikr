c7ee9bca1ab62b662745b81d7a143ab6417ae517
Parker Couch <parkercouch@gmail.com>
2019-01-21 17:16:36 -0800

    Added alertify to make flash messages prettier

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,2 +1,13 @@
+/* global alertify */
+alertify.set('notifier', 'position', 'top-left');
+
 document.addEventListener('DOMContentLoaded', () => {
+  const errors = [...document.querySelectorAll('.error-msg')];
+  errors.forEach((error) => {
+    alertify.notify(error.textContent, 'error', 5, () => {});
+  });
+  const successes = [...document.querySelectorAll('.success-msg')];
+  successes.forEach((success) => {
+    alertify.notify(success.textContent, 'success', 5, () => {});
+  });
 });
